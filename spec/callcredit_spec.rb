f763086ebab1978efc788f50509112aab038b66c
Harry Marr <harry.marr@gmail.com>
2014-02-10 15:51:16 +0000

    Make Client.config public

--- spec/callcredit_spec.rb
+++ spec/callcredit_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Callcredit do
   describe '#configure' do
-    subject { Callcredit.send(:config) }
+    subject { Callcredit.config }
     Callcredit::Config::DEFAULT_OPTIONS.keys.map(&:to_sym).each do |key|
       context "setting #{key}" do
         before { Callcredit.configure { |config| config[key] = key } }
