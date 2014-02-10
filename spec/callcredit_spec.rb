ce897eb7273e3ebe7eee33af5a8a1077fd52da70
Grey Baker <greysteil@gmail.com>
2014-02-10 16:42:00 +0000

    Require configuration, and make config accessible from client

--- spec/callcredit_spec.rb
+++ spec/callcredit_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Callcredit do
+  before { Callcredit.instance_variable_set(:@config, nil) }
+
   describe '#configure' do
     subject { Callcredit.config }
     Callcredit::Config::DEFAULT_OPTIONS.keys.map(&:to_sym).each do |key|
@@ -11,7 +13,16 @@ describe Callcredit do
     end
   end
 
+  describe "#config" do
+    subject(:config) { Callcredit.config }
+
+    it "raises a CallcreditError if Callcredit hasn't been configured" do
+      expect { config }.to raise_error Callcredit::CallcreditError
+    end
+  end
+
   describe '#id_enhanced_check' do
+    before { configure_callcredit }
     let(:data) { { first_name: "Grey", last_name: "Baker" } }
 
     it "delegates to the client" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'callcredit'
 require 'webmock/rspec'
 RSpec.configure { |config| config.include WebMock::API }
 
+def configure_callcredit
+  Callcredit.configure { |config| config[:first_name] = "Grey" }
+end
+
 shared_examples "it validates presence" do |property|
   context "with a missing #{property}" do
     before { check_data.delete(property) }
